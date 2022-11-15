8733d7b541983e6699b4421c8901e9846aa9fd14
Philip <phil.longhurst@onqor.co.uk>
2022-11-15 17:26:51 +0000

    amends

---
+++ frontend/components/UI/BackButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div @click="goTop()" class="back-button">
+        Back to top
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BackButton',
+        props: {
+            text: String,
+        },
+        methods: {
+            goTop() {
+                window.scrollTo({top: 0, behavior: 'smooth'});
+            },
+        }
+    }
+</script>

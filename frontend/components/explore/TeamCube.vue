bc8d3b81280207529fc89de9ea2551c56177621d
Philip <phil.longhurst@onqor.co.uk>
2022-11-17 16:40:43 +0000

    funky animation on hover for team, + JS added from codepen

--- frontend/components/explore/TeamCube.vue
+++ frontend/components/explore/TeamCube.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="meet-the-team__cube-holder" :class="{ active: onTheEdge , goingLeft: goingLeft, goingRight: goingRight }">
-        <div class="meet-the-team__cube">
-            <div class="cube">
+    <div class="meet-the-team__cube-holder" 
+    :class="{ active: onTheEdge , goingLeft: goingLeft, goingRight: goingRight }" >
+        <div class="meet-the-team__cube" >
+            <div class="cube" >
                 <div class="face top"></div>
                 <div class="face bottom"></div>
                 <div class="face right"></div>
                 <div class="face left"></div>
-                <img :src="$store.state.apiroute.url + teamInfo.teamAvatar.data.attributes.formats.thumbnail.url" />
+                    <img 
+                    :src="$store.state.apiroute.url + teamInfo.teamAvatar.data.attributes.formats.thumbnail.url" />
                 <div class="cube-bottom-shaddow"></div>
             </div>
             <div class="meet-the-team__info">  
@@ -17,6 +19,7 @@
                 <p>{{ teamInfo.jobTitle }}</p>
             </div>
         </div>
+           
     </div>
 </template>
 
@@ -39,6 +42,12 @@
                 onTheEdge: false,
                 goingLeft: false,
                 goingRight: false,
+                // hover animation for heads
+                // width: 0,
+                // height: 0,
+                // mouseX: 0,
+                // mouseY: 0,
+                // mouseLeaveDelay: null
             }
         },
 
@@ -58,6 +67,10 @@
             }else{
                 this.goingLeft = false
             }
+
+            // hover animation for heads
+            // this.width = this.$refs.card.offsetWidth;
+            // this.height = this.$refs.card.offsetHeight;
         },
         watch: {
             activeCube: function (val) {
@@ -88,6 +101,53 @@
                 }
         
             },
-        }
+        },
+        // methods: {
+        //     // hover animation for heads
+        //     // handleMouseMove(e) {
+        //     //     this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width/2;
+        //     //     this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height/2;
+        //     //     console.log('move')
+        //     // },
+        //     // handleMouseEnter() {
+        //     //     clearTimeout(this.mouseLeaveDelay);
+        //     //     console.log('enter')
+        //     // },
+        //     // handleMouseLeave() {
+        //     //     this.mouseLeaveDelay = setTimeout(()=>{
+        //     //         this.mouseX = 0;
+        //     //         this.mouseY = 0;
+        //     //     }, 1000);
+        //     //     console.log('leave')
+        //     // }
+        // },
+        // hover animation for heads
+        // computed: {
+        //     mousePX() {
+        //         return this.mouseX / this.width;
+        //     },
+        //     mousePY() {
+        //         return this.mouseY / this.height;
+        //     },
+        //     cardStyle() {
+        //         const rX = this.mousePX * 50;
+        //         const rY = this.mousePY * -50;
+        //         return {
+        //             transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
+        //         };
+        //     },
+        //     cardBgTransform() {
+        //         const tX = this.mousePX * -40;
+        //         const tY = this.mousePY * -40;
+        //         return {
+        //             transform: `translateX(${tX}px) translateY(${tY}px)`
+        //         }
+        //     },
+        //     cardBgImage() {
+        //         return {
+        //             backgroundImage: `url(${this.dataImage})`
+        //         }
+        //     }
+        // },
     }
 </script>

2a55f19c38e43ccce784b3f80bfbd8e2d000dc03
Marley Nicolson <marley.nicolson@onqor.co.uk>
2022-11-18 09:47:56 +0000

    service page changes

--- frontend/components/text-content/ContentColumns.vue
+++ frontend/components/text-content/ContentColumns.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <h2 v-if="this.blockData.title" class="content-columns__title">{{ this.blockData.title }}</h2>
+        <div class="content-columns__tag-line" :class="this.blockData.tagLineWidth" >
+            <h3 :class="this.blockData.tagLineWidth"> {{ this.blockData.contentTagLine }} </h3>
+        </div>
         <div class="content-columns">
             <div class="content-columns__block" v-for="(item, index) in this.blockData.content_and_columns" :key="index" :class="[{ contentImage: item.image.data }, item.columnWidth, item.centerImg ? 'center-img' : '']" >   
                 <vue-markdown v-if="item.image.data === null" :source="item.content" />
                 <img  :class="[
                 item.animate ? 'anim-spin' : '',
                 item.imageWidth ? 'image-bleed' : '']"
-                 v-if="item.image.data" :src="$store.state.apiroute.url + item.image.data.attributes.url" :style="{width: item.imageWidth + '%'}" />
+                v-if="item.image.data" :src="$store.state.apiroute.url + item.image.data.attributes.url" :style="{width: item.imageWidth + '%'}" />
                 <!-- {{ item.content }} -->
             </div>
         </div>

7d854edaed054a0c0c1a46542347a2c717bd8249
Philip <phil.longhurst@onqor.co.uk>
2022-11-28 16:17:38 +0000

    amends + img component

---
+++ frontend/components/image/Image.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img class="component-img" :src="this.$store.state.apiroute.url + this.imgUrl" />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Image',
+    props: {
+        imgUrl: String,
+    },
+}
+</script>
+
+

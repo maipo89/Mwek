9212d18e99da6255efa8421123c88f8dbe6d44b2
root <root@mwek-vm.europe-west2-a.c.onqor-1521473989687.internal>
2023-02-22 16:07:36 +0000

    server

--- frontend/.nuxt/components/index.js
+++ frontend/.nuxt/components/index.js
@@ -1,45 +1,45 @@
-export { default as BlockBuilder } from '../../components/BlockBuilder.vue'
-export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
-export { default as SiteIntro } from '../../components/SiteIntro.vue'
-export { default as Tutorial } from '../../components/Tutorial.vue'
-export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
-export { default as OnqorSEO } from '../../components/SEO/OnqorSEO.vue'
-export { default as BackButton } from '../../components/UI/BackButton.vue'
-export { default as BlogCard } from '../../components/UI/BlogCard.vue'
-export { default as DropDown } from '../../components/UI/DropDown.vue'
-export { default as Icon } from '../../components/UI/Icon.vue'
-export { default as OnqorButton } from '../../components/UI/OnqorButton.vue'
-export { default as OnqorCircleIcon } from '../../components/UI/OnqorCircleIcon.vue'
-export { default as OnqorInput } from '../../components/UI/OnqorInput.vue'
-export { default as TypeText } from '../../components/UI/TypeText.vue'
-export { default as BlogPage } from '../../components/blog/BlogPage.vue'
-export { default as SubscriptionSection } from '../../components/blog/SubscriptionSection.vue'
-export { default as CardsWithFilter } from '../../components/archives/CardsWithFilter.vue'
-export { default as CityLights } from '../../components/citymap/CityLights.vue'
-export { default as CityMapCards } from '../../components/citymap/CityMapCards.vue'
-export { default as CityMapSVG } from '../../components/citymap/CityMapSVG.vue'
-export { default as CityParks } from '../../components/citymap/CityParks.vue'
-export { default as CityRoadOne } from '../../components/citymap/CityRoadOne.vue'
-export { default as CitySculptur } from '../../components/citymap/CitySculptur.vue'
-export { default as CityTurbines } from '../../components/citymap/CityTurbines.vue'
-export { default as ContactFooter } from '../../components/contact/ContactFooter.vue'
-export { default as MeetTheTeam } from '../../components/explore/MeetTheTeam.vue'
-export { default as OurApproach } from '../../components/explore/OurApproach.vue'
-export { default as OurApproachSection } from '../../components/explore/OurApproachSection.vue'
-export { default as TeamCube } from '../../components/explore/TeamCube.vue'
-export { default as Footer } from '../../components/global/Footer.vue'
-export { default as Header } from '../../components/global/Header.vue'
-export { default as ServicePageLinks } from '../../components/services/ServicePageLinks.vue'
-export { default as HeroSection } from '../../components/image/HeroSection.vue'
-export { default as StatTabs } from '../../components/stats/StatTabs.vue'
-export { default as TabbedContent } from '../../components/tabs/TabbedContent.vue'
-export { default as ContentColumns } from '../../components/text-content/ContentColumns.vue'
-export { default as ContentWithImage } from '../../components/text-content/ContentWithImage.vue'
-export { default as Quote } from '../../components/text-content/Quote.vue'
-export { default as TextSection } from '../../components/text-content/TextSection.vue'
-export { default as ThreeDObject } from '../../components/text-content/ThreeDObject.vue'
-export { default as ThreeDObjectContent } from '../../components/text-content/ThreeDObjectContent.vue'
-export { default as TickList } from '../../components/text-content/TickList.vue'
+export const BlockBuilder = () => import('../../components/BlockBuilder.vue' /* webpackChunkName: "components/block-builder" */).then(c => wrapFunctional(c.default || c))
+export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const SiteIntro = () => import('../../components/SiteIntro.vue' /* webpackChunkName: "components/site-intro" */).then(c => wrapFunctional(c.default || c))
+export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
+export const VuetifyLogo = () => import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/vuetify-logo" */).then(c => wrapFunctional(c.default || c))
+export const OnqorSEO = () => import('../../components/SEO/OnqorSEO.vue' /* webpackChunkName: "components/onqor-s-e-o" */).then(c => wrapFunctional(c.default || c))
+export const BackButton = () => import('../../components/UI/BackButton.vue' /* webpackChunkName: "components/back-button" */).then(c => wrapFunctional(c.default || c))
+export const BlogCard = () => import('../../components/UI/BlogCard.vue' /* webpackChunkName: "components/blog-card" */).then(c => wrapFunctional(c.default || c))
+export const DropDown = () => import('../../components/UI/DropDown.vue' /* webpackChunkName: "components/drop-down" */).then(c => wrapFunctional(c.default || c))
+export const Icon = () => import('../../components/UI/Icon.vue' /* webpackChunkName: "components/icon" */).then(c => wrapFunctional(c.default || c))
+export const OnqorButton = () => import('../../components/UI/OnqorButton.vue' /* webpackChunkName: "components/onqor-button" */).then(c => wrapFunctional(c.default || c))
+export const OnqorCircleIcon = () => import('../../components/UI/OnqorCircleIcon.vue' /* webpackChunkName: "components/onqor-circle-icon" */).then(c => wrapFunctional(c.default || c))
+export const OnqorInput = () => import('../../components/UI/OnqorInput.vue' /* webpackChunkName: "components/onqor-input" */).then(c => wrapFunctional(c.default || c))
+export const TypeText = () => import('../../components/UI/TypeText.vue' /* webpackChunkName: "components/type-text" */).then(c => wrapFunctional(c.default || c))
+export const CardsWithFilter = () => import('../../components/archives/CardsWithFilter.vue' /* webpackChunkName: "components/cards-with-filter" */).then(c => wrapFunctional(c.default || c))
+export const BlogPage = () => import('../../components/blog/BlogPage.vue' /* webpackChunkName: "components/blog-page" */).then(c => wrapFunctional(c.default || c))
+export const SubscriptionSection = () => import('../../components/blog/SubscriptionSection.vue' /* webpackChunkName: "components/subscription-section" */).then(c => wrapFunctional(c.default || c))
+export const CityLights = () => import('../../components/citymap/CityLights.vue' /* webpackChunkName: "components/city-lights" */).then(c => wrapFunctional(c.default || c))
+export const CityMapCards = () => import('../../components/citymap/CityMapCards.vue' /* webpackChunkName: "components/city-map-cards" */).then(c => wrapFunctional(c.default || c))
+export const CityMapSVG = () => import('../../components/citymap/CityMapSVG.vue' /* webpackChunkName: "components/city-map-s-v-g" */).then(c => wrapFunctional(c.default || c))
+export const CityParks = () => import('../../components/citymap/CityParks.vue' /* webpackChunkName: "components/city-parks" */).then(c => wrapFunctional(c.default || c))
+export const CityRoadOne = () => import('../../components/citymap/CityRoadOne.vue' /* webpackChunkName: "components/city-road-one" */).then(c => wrapFunctional(c.default || c))
+export const CitySculptur = () => import('../../components/citymap/CitySculptur.vue' /* webpackChunkName: "components/city-sculptur" */).then(c => wrapFunctional(c.default || c))
+export const CityTurbines = () => import('../../components/citymap/CityTurbines.vue' /* webpackChunkName: "components/city-turbines" */).then(c => wrapFunctional(c.default || c))
+export const ContactFooter = () => import('../../components/contact/ContactFooter.vue' /* webpackChunkName: "components/contact-footer" */).then(c => wrapFunctional(c.default || c))
+export const MeetTheTeam = () => import('../../components/explore/MeetTheTeam.vue' /* webpackChunkName: "components/meet-the-team" */).then(c => wrapFunctional(c.default || c))
+export const OurApproach = () => import('../../components/explore/OurApproach.vue' /* webpackChunkName: "components/our-approach" */).then(c => wrapFunctional(c.default || c))
+export const OurApproachSection = () => import('../../components/explore/OurApproachSection.vue' /* webpackChunkName: "components/our-approach-section" */).then(c => wrapFunctional(c.default || c))
+export const TeamCube = () => import('../../components/explore/TeamCube.vue' /* webpackChunkName: "components/team-cube" */).then(c => wrapFunctional(c.default || c))
+export const Footer = () => import('../../components/global/Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
+export const Header = () => import('../../components/global/Header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
+export const HeroSection = () => import('../../components/image/HeroSection.vue' /* webpackChunkName: "components/hero-section" */).then(c => wrapFunctional(c.default || c))
+export const ServicePageLinks = () => import('../../components/services/ServicePageLinks.vue' /* webpackChunkName: "components/service-page-links" */).then(c => wrapFunctional(c.default || c))
+export const StatTabs = () => import('../../components/stats/StatTabs.vue' /* webpackChunkName: "components/stat-tabs" */).then(c => wrapFunctional(c.default || c))
+export const TabbedContent = () => import('../../components/tabs/TabbedContent.vue' /* webpackChunkName: "components/tabbed-content" */).then(c => wrapFunctional(c.default || c))
+export const ContentColumns = () => import('../../components/text-content/ContentColumns.vue' /* webpackChunkName: "components/content-columns" */).then(c => wrapFunctional(c.default || c))
+export const ContentWithImage = () => import('../../components/text-content/ContentWithImage.vue' /* webpackChunkName: "components/content-with-image" */).then(c => wrapFunctional(c.default || c))
+export const Quote = () => import('../../components/text-content/Quote.vue' /* webpackChunkName: "components/quote" */).then(c => wrapFunctional(c.default || c))
+export const TextSection = () => import('../../components/text-content/TextSection.vue' /* webpackChunkName: "components/text-section" */).then(c => wrapFunctional(c.default || c))
+export const ThreeDObject = () => import('../../components/text-content/ThreeDObject.vue' /* webpackChunkName: "components/three-d-object" */).then(c => wrapFunctional(c.default || c))
+export const ThreeDObjectContent = () => import('../../components/text-content/ThreeDObjectContent.vue' /* webpackChunkName: "components/three-d-object-content" */).then(c => wrapFunctional(c.default || c))
+export const TickList = () => import('../../components/text-content/TickList.vue' /* webpackChunkName: "components/tick-list" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
--- frontend/.nuxt/index.js
+++ frontend/.nuxt/index.js
@@ -13,11 +13,11 @@ import { createStore } from './store.js'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_67ddceda from 'nuxt_plugin_plugin_67ddceda' // Source: ./components/plugin.js (mode: 'all')
-import nuxt_plugin_plugin_c5f51958 from 'nuxt_plugin_plugin_c5f51958' // Source: ./vuetify/plugin.js (mode: 'all')
-import nuxt_plugin_axios_5cebf0f0 from 'nuxt_plugin_axios_5cebf0f0' // Source: ./axios.js (mode: 'all')
+import nuxt_plugin_plugin_5b4e58fb from 'nuxt_plugin_plugin_5b4e58fb' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_plugin_36d904d3 from 'nuxt_plugin_plugin_36d904d3' // Source: ./vuetify/plugin.js (mode: 'all')
+import nuxt_plugin_axios_1449e491 from 'nuxt_plugin_axios_1449e491' // Source: ./axios.js (mode: 'all')
 import nuxt_plugin_gsap_6a80fe91 from 'nuxt_plugin_gsap_6a80fe91' // Source: ../plugins/gsap (mode: 'client')
-import nuxt_plugin_distplugin71ae783d_02203d76 from 'nuxt_plugin_distplugin71ae783d_02203d76' // Source: ./dist.plugin.71ae783d.js (mode: 'all')
+import nuxt_plugin_distplugin8d6c0108_5e138292 from 'nuxt_plugin_distplugin8d6c0108_5e138292' // Source: ./dist.plugin.8d6c0108.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -214,24 +214,24 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_67ddceda === 'function') {
-    await nuxt_plugin_plugin_67ddceda(app.context, inject)
+  if (typeof nuxt_plugin_plugin_5b4e58fb === 'function') {
+    await nuxt_plugin_plugin_5b4e58fb(app.context, inject)
   }
 
-  if (typeof nuxt_plugin_plugin_c5f51958 === 'function') {
-    await nuxt_plugin_plugin_c5f51958(app.context, inject)
+  if (typeof nuxt_plugin_plugin_36d904d3 === 'function') {
+    await nuxt_plugin_plugin_36d904d3(app.context, inject)
   }
 
-  if (typeof nuxt_plugin_axios_5cebf0f0 === 'function') {
-    await nuxt_plugin_axios_5cebf0f0(app.context, inject)
+  if (typeof nuxt_plugin_axios_1449e491 === 'function') {
+    await nuxt_plugin_axios_1449e491(app.context, inject)
   }
 
   if (process.client && typeof nuxt_plugin_gsap_6a80fe91 === 'function') {
     await nuxt_plugin_gsap_6a80fe91(app.context, inject)
   }
 
-  if (typeof nuxt_plugin_distplugin71ae783d_02203d76 === 'function') {
-    await nuxt_plugin_distplugin71ae783d_02203d76(app.context, inject)
+  if (typeof nuxt_plugin_distplugin8d6c0108_5e138292 === 'function') {
+    await nuxt_plugin_distplugin8d6c0108_5e138292(app.context, inject)
   }
 
   // Lock enablePreview in context

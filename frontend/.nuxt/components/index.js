919e6bd76b08b6bb80238ea281311f296e7a757f
root <root@mwek-vm.europe-west2-a.c.onqor-1521473989687.internal>
2023-03-15 12:24:21 +0000

    sitemap addtion

--- frontend/.nuxt/components/index.js
+++ frontend/.nuxt/components/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const BlockBuilder = () => import('../../components/BlockBuilder.vue' /* webpackChunkName: "components/block-builder" */).then(c => wrapFunctional(c.default || c))
 export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
 export const SiteIntro = () => import('../../components/SiteIntro.vue' /* webpackChunkName: "components/site-intro" */).then(c => wrapFunctional(c.default || c))
@@ -41,50 +40,6 @@ export const TextSection = () => import('../../components/text-content/TextSecti
 export const ThreeDObject = () => import('../../components/text-content/ThreeDObject.vue' /* webpackChunkName: "components/three-d-object" */).then(c => wrapFunctional(c.default || c))
 export const ThreeDObjectContent = () => import('../../components/text-content/ThreeDObjectContent.vue' /* webpackChunkName: "components/three-d-object-content" */).then(c => wrapFunctional(c.default || c))
 export const TickList = () => import('../../components/text-content/TickList.vue' /* webpackChunkName: "components/tick-list" */).then(c => wrapFunctional(c.default || c))
-=======
-export { default as BlockBuilder } from '../../components/BlockBuilder.vue'
-export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
-export { default as SiteIntro } from '../../components/SiteIntro.vue'
-export { default as Tutorial } from '../../components/Tutorial.vue'
-export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
-export { default as BackButton } from '../../components/UI/BackButton.vue'
-export { default as BlogCard } from '../../components/UI/BlogCard.vue'
-export { default as DropDown } from '../../components/UI/DropDown.vue'
-export { default as Icon } from '../../components/UI/Icon.vue'
-export { default as OnqorButton } from '../../components/UI/OnqorButton.vue'
-export { default as OnqorCircleIcon } from '../../components/UI/OnqorCircleIcon.vue'
-export { default as OnqorInput } from '../../components/UI/OnqorInput.vue'
-export { default as TypeText } from '../../components/UI/TypeText.vue'
-export { default as CardsWithFilter } from '../../components/archives/CardsWithFilter.vue'
-export { default as CityLights } from '../../components/citymap/CityLights.vue'
-export { default as CityMapCards } from '../../components/citymap/CityMapCards.vue'
-export { default as CityMapSVG } from '../../components/citymap/CityMapSVG.vue'
-export { default as CityParks } from '../../components/citymap/CityParks.vue'
-export { default as CityRoadOne } from '../../components/citymap/CityRoadOne.vue'
-export { default as CitySculptur } from '../../components/citymap/CitySculptur.vue'
-export { default as CityTurbines } from '../../components/citymap/CityTurbines.vue'
-export { default as BlogPage } from '../../components/blog/BlogPage.vue'
-export { default as SubscriptionSection } from '../../components/blog/SubscriptionSection.vue'
-export { default as MeetTheTeam } from '../../components/explore/MeetTheTeam.vue'
-export { default as OurApproach } from '../../components/explore/OurApproach.vue'
-export { default as OurApproachSection } from '../../components/explore/OurApproachSection.vue'
-export { default as TeamCube } from '../../components/explore/TeamCube.vue'
-export { default as Footer } from '../../components/global/Footer.vue'
-export { default as Header } from '../../components/global/Header.vue'
-export { default as HeroSection } from '../../components/image/HeroSection.vue'
-export { default as OnqorSEO } from '../../components/SEO/OnqorSEO.vue'
-export { default as ContactFooter } from '../../components/contact/ContactFooter.vue'
-export { default as ContentColumns } from '../../components/text-content/ContentColumns.vue'
-export { default as ContentWithImage } from '../../components/text-content/ContentWithImage.vue'
-export { default as Quote } from '../../components/text-content/Quote.vue'
-export { default as TextSection } from '../../components/text-content/TextSection.vue'
-export { default as ThreeDObject } from '../../components/text-content/ThreeDObject.vue'
-export { default as ThreeDObjectContent } from '../../components/text-content/ThreeDObjectContent.vue'
-export { default as TickList } from '../../components/text-content/TickList.vue'
-export { default as ServicePageLinks } from '../../components/services/ServicePageLinks.vue'
-export { default as StatTabs } from '../../components/stats/StatTabs.vue'
-export { default as TabbedContent } from '../../components/tabs/TabbedContent.vue'
->>>>>>> 79903b108cdaff8915063ee4579703795021da46
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {

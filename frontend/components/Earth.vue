ae80ff3f3b59d3d2a231ead074d4efdbc2247c3a
marcobrunettionqor <marco.brunetti@onqor.co.uk>
2023-03-31 13:55:31 +0100

    Earth spinning - gltf file - correct colors

--- frontend/components/Earth.vue
+++ frontend/components/Earth.vue
@@ -9,6 +9,7 @@ import * as Three from 'three'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export default {
   name: 'Earth',
@@ -34,67 +35,44 @@ export default {
         var ambientLight = new Three.AmbientLight(0xffffff, 0.5)
         scene.add(ambientLight)
 
-        // var directionalLight = new Three.DirectionalLight( 0xffffff, 1 );
-        // scene.add( directionalLight );
+        var directionalLight = new Three.DirectionalLight( 0xffffff, 1 );
+        // directionalLight.color.setRGB( 0, 0, 0 );
+        directionalLight.position.set( 1, 1, 1 );
+        scene.add( directionalLight );
 
-        var light = new Three.PointLight( 0xffffff, 1, 10000 );
-        light.position.set( 0, 100, 600);
-        scene.add( light );
-
-        // var light = new Three.AmbientLight( 0xffffff ); // soft white light
+        // var light = new Three.PointLight( 0xffffff, 1, 100000 );
+        // light.position.set( 0, 0, 200);
         // scene.add( light );
+        
+        const loader = new GLTFLoader();
 
+        loader.load( 'images/3D/Earth.gltf', function ( gltf ) {
+            model = gltf.scene
+            model.position.x =  0.3;
+            model.rotation.set(0, -5, 0)
+            gsap.to(model.rotation, {
+                y: -4.79,
+                scrollTrigger: {
+                trigger: "#container",
+                scroller: '#page-modal',
+                start: "-390px 50%",
+                end: "bottom top",
+                scrub: true,
+                toggleActions: "restart pause resume pause"
+            },
+                y: Math.PI
+            });
 
-        new EXRLoader()
-			.load( '/images/3D/hdri.exr', function ( texture, textureData ) {
+            scene.add( gltf.scene );
 
-						// memorial.exr is NPOT
+        }, undefined, function ( error ) {
 
-						//console.log( textureData );
-						//console.log( texture );
+            console.error( error );
 
-						// EXRLoader sets these default settings
-						//texture.generateMipmaps = false;
-						//texture.minFilter = LinearFilter;
-						//texture.magFilter = LinearFilter;
-
-                        texture.mapping = Three.EquirectangularReflectionMapping;
-
-                        const manager = new Three.LoadingManager();
-
-						var mtlLoader = new MTLLoader();
-                        mtlLoader.load('/images/3D/earth.mtl', function(materials) {
-                            materials.preload();
-                            var objLoader = new OBJLoader();
-                            objLoader.setMaterials( materials );
-                            objLoader.load('/images/3D/earth.obj', function(object) {
-                                model = object
-                                model.receiveShadow = true;
-                                model.traverse( function( child ) {
-									if ( child.isMesh ) child.material.envMap = texture;
-									
-								} );
-                                model.position.x =  0.3;
-                                model.rotation.set(0, -10, 0)
-                                gsap.to(model.rotation, {
-                                    y: -4.79,
-                                    scrollTrigger: {
-                                    trigger: "#container",
-                                    scroller: '#page-modal',
-                                    start: "-390px 50%",
-                                    end: "bottom top",
-                                    scrub: true,
-                                    toggleActions: "restart pause resume pause"
-                                },
-                                    y: Math.PI
-                                });
-                                scene.add(model)
-                            });
-                        });
-
-			} );
+        } );
 
         renderer = new Three.WebGLRenderer({antialias: true});
+        renderer.outputEncoding = Three.sRGBEncoding;
         renderer.setSize(container.clientWidth, container.clientHeight);
         container.appendChild(renderer.domElement);
 

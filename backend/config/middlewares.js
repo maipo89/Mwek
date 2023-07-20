bff7ac1e35084338708fbdc62c9b0647567d5ff6
Marley Nicolson <marley.nicolson@onqor.co.uk>
2023-07-20 09:53:48 +0100

    trying to increase upload size

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -6,17 +6,16 @@ module.exports = [
   'strapi::logger',
   'strapi::query',
   {
-    name: "strapi::body",
+    name:'strapi::body',
     config: {
       formLimit: "256mb", // modify form body
       jsonLimit: "256mb", // modify JSON body
       textLimit: "256mb", // modify text body
       formidable: {
-        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
       },
     },
   },
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
